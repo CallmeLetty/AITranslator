0d57daafcdc0d47b3cd2e7fda5f4fbdd8fead1d7
金泉斌 <jinquanbin@jinquanbindeMac-mini.local>
2023-05-09 00:23:20 +0800

    fea: import threadpool

--- AITranslator/Translator.py
+++ AITranslator/Translator.py
@@ -1,29 +1,55 @@
 from Requestor import Requestor
-from Prompt import Prompt
 import json, os
+from collections import deque
+import threadpool
+import threading
 
 class Translator:
     """backEnd"""
-    def __init__(self):
-        self.__file_path = os.path.join(os.getcwd(), 'output.json')
+
+    def __init__(self, output: str = None):
+        """
+        Parameters
+        ----------
+        output
+            指定输出地址
+        """
+        if not output:
+            self.file_path = os.path.join(os.getcwd(), 'output.json')
+        else:
+            self.file_path = output
+
         self.__requestor = Requestor()
         self.index = 0
-        pass
+        self.queue = deque()
+        self.__pool = threadpool.ThreadPool(9) # 核心数 + 1
 
     def sink(self, index):
         self.index = index
 
-    """metaData tuple (key, source)"""
-    def config(self, metaData: list, langs: list[str]):
-        """配置元数据和翻译语种"""
+    def config(self, metaData: list[(str, str)], langs: list[str]):
+        """配置元数据和翻译语种
+
+        Parameters
+        ----------
+        metaData
+            待翻译数据，(key, source)的元祖列表
+        langs
+            输出的语种
+        """
         self.metaData = metaData
         self.langs = langs
     
     def translate(self) -> str:
-        """批量翻译， 返回json文件路径"""
-        ## 逐条翻译（Loop）
+        """执行翻译
+                
+        Retruns
+        ----------
+        翻译结果的json文件路径
+        """
         result = []
         count = len(self.metaData)
+        ## 逐条翻译（Loop）
         for i in range(self.index, count):
             key, text = self.metaData[i]
             res = self.__translate(text)
@@ -32,11 +58,15 @@ class Translator:
             self.index += 1
 
         self.__writeToFile()
-        return self.__file_path
+
+        return self.file_path
+    
+    def __callback(request, result):
+        pass
     
     def __writeToFile(self, data):
         """写入到文件中"""
-        with open(self.__file_path, 'w') as f:
+        with open(self.file_path, 'w') as f:
             json.dump(data, f, allow_unicode=True)
 
     def __translate(self, entry: str) -> dict:
